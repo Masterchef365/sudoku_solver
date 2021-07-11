9c93b9a55e7015b05f3784c484d1ede67d1a0d5e
Masterchef365 <duncan.freeman1@gmail.com>
2021-07-11 02:15:30 -0700

    Puzzle check

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,7 @@ fn check_block(block: Block, test_zero: bool) -> bool {
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
     fn test_check_block() {
         assert!(check_block([1, 2, 3, 4, 5, 6, 7, 8, 9], true));
         assert!(!check_block([1, 2, 3, 4, 5, 6, 7, 8, 10], true));
@@ -92,7 +93,9 @@ mod tests {
         assert!(check_block([1, 2, 3, 4, 5, 6, 7, 8, 0], false));
     }
 
+    #[test]
     fn test_check_puzzle() {
+        // True puzzle
         let data = [
             5, 3, 4, 6, 7, 8, 9, 1, 2,
             6, 7, 2, 1, 9, 5, 3, 4, 8,
@@ -105,5 +108,39 @@ mod tests {
             3, 4, 5, 2, 8, 6, 1, 7, 9,
         ];
         let puzzle = Puzzle { data };
+        assert!(check_puzzle(&puzzle, true));
+        assert!(check_puzzle(&puzzle, false));
+
+        // One part empty
+        let data = [
+            5, 3, 4, 6, 7, 8, 9, 1, 2,
+            6, 7, 2, 1, 9, 5, 3, 4, 8,
+            1, 9, 8, 3, 4, 2, 5, 6, 7,
+            8, 5, 9, 7, 6, 1, 4, 2, 3,
+            4, 2, 6, 8, 5, 0, 7, 9, 1,
+            7, 1, 3, 9, 2, 4, 8, 5, 6,
+            9, 6, 1, 5, 3, 7, 2, 8, 4,
+            2, 8, 7, 4, 1, 9, 6, 3, 5,
+            3, 4, 5, 2, 8, 6, 1, 7, 9,
+        ];
+        let puzzle = Puzzle { data };
+        assert!(!check_puzzle(&puzzle, true));
+        assert!(check_puzzle(&puzzle, false));
+
+        // Transposed two elements
+        let data = [
+            5, 7, 4, 6, 7, 8, 9, 1, 2,
+            6, 3, 2, 1, 9, 5, 3, 4, 8,
+            1, 9, 8, 3, 4, 2, 5, 6, 7,
+            8, 5, 9, 7, 6, 1, 4, 2, 3,
+            4, 2, 6, 8, 5, 3, 7, 9, 1,
+            7, 1, 3, 9, 2, 4, 8, 5, 6,
+            9, 6, 1, 5, 3, 7, 2, 8, 4,
+            2, 8, 7, 4, 1, 9, 6, 3, 5,
+            3, 4, 5, 2, 8, 6, 1, 7, 9,
+        ];
+        let puzzle = Puzzle { data };
+        assert!(!check_puzzle(&puzzle, true));
+        assert!(!check_puzzle(&puzzle, false));
     }
 }
