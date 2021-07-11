3b6b8884f50b3cf0586d091ae8c11c64052f901b
Masterchef365 <duncan.freeman1@gmail.com>
2021-07-11 02:12:40 -0700

    Write out test puzzle

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ struct Puzzle {
     data: [u8; 9*9],
 }
 
-fn check_puzzle(puzzle: &Puzzle) -> bool {
+fn check_puzzle(puzzle: &Puzzle, test_zero: bool) -> bool {
     // Check rows:
     for row in puzzle.data.chunks_exact(9) {
         let mut block = [0u8; 9];
         block.copy_from_slice(row);
-        if !check_block(block) {
+        if !check_block(block, test_zero) {
             return false;
         }
     }
@@ -23,7 +23,7 @@ fn check_puzzle(puzzle: &Puzzle) -> bool {
         for (&puzz, block) in puzzle.data.iter().skip(i).step_by(9).zip(&mut block) {
             *block = puzz;
         }
-        if !check_block(block) {
+        if !check_block(block, test_zero) {
             return false;
         }
     }
@@ -39,7 +39,7 @@ fn check_puzzle(puzzle: &Puzzle) -> bool {
                     i += 1;
                 }
             }
-            if !check_block(block) {
+            if !check_block(block, test_zero) {
                 return false;
             }
         }
@@ -50,9 +50,17 @@ fn check_puzzle(puzzle: &Puzzle) -> bool {
 
 type Block = [u8; 9];
 
-fn check_block(block: Block) -> bool {
+fn check_block(block: Block, test_zero: bool) -> bool {
     let mut bits = [false; 10]; // Could also do -1 but I figure this may be faster... 
     for &elem in &block {
+        if elem == 0 {
+            if test_zero {
+                return false;
+            } else {
+                continue;
+            }
+        }
+
         let bit = match bits.get_mut(elem as usize) {
             Some(b) => b,
             None => return false,
@@ -71,13 +79,31 @@ fn check_block(block: Block) -> bool {
 mod tests {
     use super::*;
     fn test_check_block() {
-        assert!(check_block([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-        assert!(!check_block([1, 2, 3, 4, 5, 6, 7, 8, 10]));
-        assert!(!check_block([2, 2, 3, 4, 5, 6, 7, 8, 9]));
-        assert!(check_block([2, 1, 3, 4, 5, 6, 7, 8, 9]));
-        assert!(check_block([4, 1, 3, 2, 5, 6, 7, 8, 9]));
-        assert!(check_block([4, 1, 3, 2, 5, 6, 9, 8, 7]));
-        assert!(check_block([4, 1, 3, 2, 6, 5, 8, 9, 7]));
-        assert!(check_block([4, 8, 3, 2, 6, 5, 1, 9, 7]));
+        assert!(check_block([1, 2, 3, 4, 5, 6, 7, 8, 9], true));
+        assert!(!check_block([1, 2, 3, 4, 5, 6, 7, 8, 10], true));
+        assert!(!check_block([2, 2, 3, 4, 5, 6, 7, 8, 9], true));
+        assert!(check_block([2, 1, 3, 4, 5, 6, 7, 8, 9], true));
+        assert!(check_block([4, 1, 3, 2, 5, 6, 7, 8, 9], true));
+        assert!(check_block([4, 1, 3, 2, 5, 6, 9, 8, 7], true));
+        assert!(check_block([4, 1, 3, 2, 6, 5, 8, 9, 7], true));
+        assert!(check_block([4, 8, 3, 2, 6, 5, 1, 9, 7], true));
+
+        assert!(!check_block([1, 2, 3, 4, 5, 6, 7, 8, 0], true));
+        assert!(check_block([1, 2, 3, 4, 5, 6, 7, 8, 0], false));
+    }
+
+    fn test_check_puzzle() {
+        let data = [
+            5, 3, 4, 6, 7, 8, 9, 1, 2,
+            6, 7, 2, 1, 9, 5, 3, 4, 8,
+            1, 9, 8, 3, 4, 2, 5, 6, 7,
+            8, 5, 9, 7, 6, 1, 4, 2, 3,
+            4, 2, 6, 8, 5, 3, 7, 9, 1,
+            7, 1, 3, 9, 2, 4, 8, 5, 6,
+            9, 6, 1, 5, 3, 7, 2, 8, 4,
+            2, 8, 7, 4, 1, 9, 6, 3, 5,
+            3, 4, 5, 2, 8, 6, 1, 7, 9,
+        ];
+        let puzzle = Puzzle { data };
     }
 }
